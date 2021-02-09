593fa7775421841a87c2ddac784972f86bab47fe
EthanPassinoFWCS <69813689+EthanPassinoFWCS@users.noreply.github.com>
2021-02-09 14:43:01 -0500

    Added the getData function

--- datahandling/data.py
+++ datahandling/data.py
@@ -1,9 +1,28 @@
+import pyodbc
+# pip install pyodbc to get this.
+
 class Data:
   def __init__(self, filename):
       self.filename = filename
+      self.getdata()
 
   def getdata(self):
       '''Gets a big list of all the data in the accessdb file. This just might be in the init later on. This will ignore student names.'''
+      '''Need https://www.microsoft.com/en-us/download/details.aspx?id=50420 driver to run'''
+      conn = pyodbc.connect("DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};" + " DBQ=data/" + self.filename)
+      cursor = conn.cursor()
+      cursor.execute("SELECT * FROM FruitSale")
+      data = []
+      self.columns = [column[0] for column in cursor.description]
+      for row in cursor.fetchall():
+          entry = [r for r in row]
+          fixed_entry = {}
+          for c in self.columns:
+              if c == "teacherCode" or c == "StudentLastName" or c == "StudentFirstName":
+                  continue
+              fixed_entry[c] = entry[self.columns.index(c)]
+          data.append(fixed_entry)
+      self.data = data
       
 
   def getFruitData(self, fruit):
