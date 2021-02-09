9adf4a3ffc1f2e1f9e5d8074216dc98133c1a218
EthanPassinoFWCS <69813689+EthanPassinoFWCS@users.noreply.github.com>
2021-02-09 13:49:56 -0500

    Update data.py

--- datahandling/data.py
+++ datahandling/data.py
@@ -1,14 +1,14 @@
 class Data:
   def __init__(self, filename):
-    self.filename = filename
+      self.filename = filename
 
   def getdata(self):
-    '''Gets a big list of all the data in the accessdb file. This just might be in the init later on. This will ignore student names.'''
+      '''Gets a big list of all the data in the accessdb file. This just might be in the init later on. This will ignore student names.'''
+      
 
   def getFruitData(self, fruit):
-    '''This goes through and gets all data about a fruit, its name and number of buys'''
-    '''self.data will contain the data'''
+      '''This goes through and gets all data about a fruit, its name and number of buys'''
+      '''self.data will contain the data'''
 
   def orderFruits(self):
-    '''This will return a list with the fruits ordered from most bought from to least bought'''
-    
+      '''This will return a list with the fruits ordered from most bought from to least bought'''
