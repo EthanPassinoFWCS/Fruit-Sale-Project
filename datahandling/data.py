3764817b9ace6e0f2c798db65b9266fd81c64da1
EthanPassinoFWCS <69813689+EthanPassinoFWCS@users.noreply.github.com>
2021-02-11 14:11:13 -0500

    Try except and instructions for error

--- datahandling/data.py
+++ datahandling/data.py
@@ -1,6 +1,6 @@
 import pyodbc
 # pip install pyodbc to get this.
-
+import sys
 class Data:
     def __init__(self, filename):
         self.filename = filename
@@ -8,8 +8,17 @@ class Data:
 
     def getdata(self):
         '''Gets a big list of all the data in the accessdb file. This just might be in the init later on. This will ignore student names.'''
-        '''Needhttps://www.microsoft.com/en-us/download/details.aspx?id=54920 to run. Make sure it is the same bit version as python. If it doesn't say which it is, it is 32 bit.'''
-        conn = pyodbc.connect("DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};" + " DBQ=data/" + self.filename + ";")
+        '''Need https://www.microsoft.com/en-us/download/details.aspx?id=54920 to run. Make sure it is the same bit version as python. If it doesn't say which it is, it is 32 bit.'''
+        try:
+            conn = pyodbc.connect("DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};" + " DBQ=data/" + self.filename + ";")
+        except pyodbc.InterfaceError:
+            print("An error has occured that has caused us not to be able to access the database file to get the data. This more than likely means that you do not have the correct stuff installed properly to access these files. Your python bit version and the bit version of the Microsoft Access Driver must be the same.")
+            is64bit = sys.maxsize > 2**32
+            if is64bit:
+                print("The python version you were running was 64 bit. Download and install the 'accessdatabaseengine_X64.exe' file from here: https://www.microsoft.com/en-us/download/details.aspx?id=54920")
+            else:
+                print("The python version you were running was 32 bit. Download and install the 'accessdatabaseengine.exe' file from here: https://www.microsoft.com/en-us/download/details.aspx?id=54920")
+            exit(-1)
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM FruitSale")
         data = []
